6bd32d410fafaf2b4f6affdcb11ae107a832c109
Alex Yan <cattelbob@gmail.com>
2024-02-22 11:52:37 -0500

    Updating user model and setting up insert endpoint

--- MatchPlayUserAPI/src/controllers/user.controller.js
+++ MatchPlayUserAPI/src/controllers/user.controller.js
@@ -1,14 +1,16 @@
 const User = require("../models/user.model");
 
-const getUserByID = async (req,res) => {
-    try {
-        const user = await User.findById(req.params.id)
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json(error);
-    }
+const getUserByID = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
+const insertUser = async (req, res) => {};
 module.exports = {
-    getUserByID
-}
+  getUserByID,
+  insertUser,
+};
