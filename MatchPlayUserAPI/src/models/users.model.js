cd8281a4fec0a7b4117646be3d35c608f878e3dd
Alex Yan <cattelbob@gmail.com>
2024-02-22 12:35:55 -0500

    updated users.model.js

--- MatchPlayUserAPI/src/models/users.model.js
+++ MatchPlayUserAPI/src/models/users.model.js
@@ -32,7 +32,10 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    playingRange: [Number],
+    playingRange: {
+      lower: { type: Number, required: true },
+      upper: { type: Number, required: true },
+    },
     drinkingSmoking: {
       drinks: { type: Boolean, required: true },
       smokes: { type: Boolean, required: true },
@@ -46,9 +49,9 @@ const userSchema = new Schema(
       none: { type: Boolean, required: true },
     },
     timePrefs: {
-      weekendDaylight: { type: Boolean, required: true },
+      weekendDaytime: { type: Boolean, required: true },
       weekendTwilight: { type: Boolean, required: true },
-      weekdayDaylight: { type: Boolean, required: true },
+      weekdayDaytime: { type: Boolean, required: true },
       weekdayTwilight: { type: Boolean, required: true },
     },
   },
