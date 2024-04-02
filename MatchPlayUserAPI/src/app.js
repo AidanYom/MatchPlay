25eff29237bf8d84b3a74b799a9ea0c78d188908
Adam He <he.adam715@gmail.com>
2024-04-02 18:43:22 -0400

    Added packages for chat

--- MatchPlayUserAPI/src/app.js
+++ MatchPlayUserAPI/src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const jwt = require("jsonwebtoken");
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
 
 const usersRoute = require("./routes/users.route");
 const likesRoute = require("./routes/likes.route");
