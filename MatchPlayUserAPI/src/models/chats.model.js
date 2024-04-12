c3fb0b184b544fb1c66fa7c469129074005e3cf4
Adam He <he.adam715@gmail.com>
2024-04-12 15:28:23 -0400

    Renamed chatSchema to messageSchema

--- MatchPlayUserAPI/src/models/chats.model.js
+++ MatchPlayUserAPI/src/models/chats.model.js
@@ -2,20 +2,23 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const chatSchema = new Schema({
-  senderId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
+const messageShema = new Schema(
+  {
+    senderId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    recepientId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    message: String,
+    timeStamp: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  recepientId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  message: String,
-  timeStamp: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { collection: "messages" }
+);
 
-module.exports = mongoose.model("Chat", chatSchema);
+module.exports = mongoose.model("Message", messageShema);
