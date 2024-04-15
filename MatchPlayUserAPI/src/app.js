2d79737e7ac00db3b71310affba4bfe919277640
Alex Yan <cattelbob@gmail.com>
2024-04-15 09:45:06 -0400

    started api API_documentation

--- MatchPlayUserAPI/src/app.js
+++ MatchPlayUserAPI/src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 app.use(cors());
@@ -12,6 +14,20 @@ app.get("/", (req, res) => {
   res.status(200).json({ alive: "True" });
 });
 
+
 app.use("/users", userRoutes);
+
+var options = {};
+
+app.use(
+  "/api-docs",
+  function (req, res, next) {
+    swaggerDocument.host = req.get("host");
+    req.swaggerDoc = swaggerDocument;
+    next();
+  },
+  swaggerUi.serveFiles(swaggerDocument, options),
+  swaggerUi.setup()
+);
 
 module.exports = app;
