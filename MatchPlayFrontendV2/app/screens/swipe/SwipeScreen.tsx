ce8a7abad84a97cd0646811c3357d65607680481
Adam He <he.adam715@gmail.com>
2024-04-17 09:10:38 -0400

    Initial swipe with dummy users

--- MatchPlayFrontendV2/app/screens/swipe/SwipeScreen.tsx
+++ MatchPlayFrontendV2/app/screens/swipe/SwipeScreen.tsx
@@ -1,11 +1,32 @@
-import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
-import React, { useContext, useEffect, useState, useLayoutEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Pressable,
+  Animated,
+  PanResponder,
+  Dimensions,
+} from "react-native";
+import React, {
+  useContext,
+  useEffect,
+  useState,
+  useLayoutEffect,
+  useRef,
+  useCallback,
+} from "react";
 import { UserType } from "../../UserContext";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import Card from "../../components/Card";
+import Footer from "../../components/Footer";
+
+const { width, height } = Dimensions.get("screen");
 
 const SwipeScreen = ({ navigation }) => {
-  const [potentialMatch, setPotentialMatch] = useState(UserType);
+  const [potentialMatch, setPotentialMatch] = useState([]);
   const { userId, setUserId } = useContext(UserType);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: "",
@@ -24,9 +45,118 @@ const SwipeScreen = ({ navigation }) => {
       ),
     });
   }, []);
+
+  useEffect(() => {
+    setUserId("660f11e2b13eaea0ac834367");
+  });
+
+  useEffect(() => {
+    const compatibleUser = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/users/${userId}/compatible`
+        );
+        const data = await response.json();
+
+        if (response.ok) {
+          setPotentialMatch(data);
+        }
+      } catch (error) {
+        console.log("Error showing other users", error);
+      }
+    };
+
+    compatibleUser();
+  }, []);
+
+  const things = [
+    { name: "Adam He", image: require("../../assets/golfer.jpeg") },
+    { name: "Alex Yan", image: require("../../assets/golfer.jpeg") },
+  ];
+
+  const [users, setUsers] = useState(things);
+
+  const swipe = useRef(new Animated.ValueXY()).current;
+  const titleSign = useRef(new Animated.Value(1)).current;
+
+  const panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+
+    onPanResponderMove: (_, { dx, dy, y0 }) => {
+      swipe.setValue({ x: dx, y: dy });
+      titleSign.setValue(y0 > (height * 0.9) / 2 ? 1 : -1);
+    },
+
+    onPanResponderRelease: (_, { dx, dy }) => {
+      const direction = Math.sign(dx);
+      const isActionActive = Math.abs(dx) > 100;
+
+      if (isActionActive) {
+        // Swipe the card off the screen
+        Animated.timing(swipe, {
+          duration: 500,
+          toValue: {
+            x: direction * 500,
+            y: dy,
+          },
+          useNativeDriver: true,
+        }).start(removeTopCard);
+      } else {
+        // Return the card to original positions
+        Animated.spring(swipe, {
+          toValue: {
+            x: 0,
+            y: 0,
+          },
+          useNativeDriver: true,
+          friction: 5,
+        }).start();
+      }
+    },
+  });
+
+  const removeTopCard = useCallback(() => {
+    setUsers((prevState) => prevState.slice(1));
+    swipe.setValue({ x: 0, y: 0 });
+  }, [swipe]);
+
+  const handleChoice = useCallback(
+    (direction) => {
+      Animated.timing(swipe.x, {
+        toValue: direction * 500,
+        duration: 400,
+        useNativeDriver: true,
+      }).start(removeTopCard);
+    },
+    [removeTopCard, swipe.x]
+  );
+
+  useEffect(() => {
+    if (!users.length) {
+      setUsers(things);
+    }
+  }, [users.length]);
+
   return (
-    <View>
-      <Text>Swipe Screen</Text>
+    <View style={{ flex: 1, alignItems: "center", backgroundColor: "white" }}>
+      {users
+        .map(({ name }, index) => {
+          const isFirst = index == 0;
+          const dragHandlers = isFirst ? panResponder.panHandlers : {};
+          return (
+            <Card
+              key={name}
+              name={name}
+              image={require("../../assets/golfer.jpeg")}
+              isFirst={isFirst}
+              swipe={swipe}
+              titleSign={titleSign}
+              {...dragHandlers}
+            />
+          );
+        })
+        .reverse()}
+      <Footer handleChoice={handleChoice} />
     </View>
   );
 };
