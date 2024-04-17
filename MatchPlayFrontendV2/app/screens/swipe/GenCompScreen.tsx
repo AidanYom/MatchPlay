b8a47239805662d3d0aede44509f366ed3dadade
Adam He <he.adam715@gmail.com>
2024-04-17 17:14:54 -0400

    Init GenCompScreen

---
+++ MatchPlayFrontendV2/app/screens/swipe/GenCompScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const GenCompScreen = () => {
+  return (
+    <View>
+      <Text>GenCompScreen</Text>
+    </View>
+  );
+};
+
+export default GenCompScreen;
